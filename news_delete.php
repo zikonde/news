9656dc23f2f12022095d8ceb7e05c261cb18c66b
Zikonde Nyirenda <103549398+zikonde@users.noreply.github.com>
2024-05-29 22:46:06 +0800

    功能完善
    完善了 login_process news_add, news_save, news_edit, news_update, news _delete 功能

--- news_delete.php
+++ news_delete.php
@@ -1,20 +1,22 @@
 <?php 
 include_once("functions/is_login.php"); 
-if (!session_id()){//这里使用session_id()判断是否已经开启了Session 
-     session_start(); 
-} 
+include_once("functions/session_config.php"); 
+
 if(!is_login()){ 
      echo "请您登录系统后，再访问该页面！"; 
      return; 
+}else{
+     include_once("functions/database.php"); 
+     $news_id = $_GET["news_id"]; 
+
+     get_connection(); 
+     mysqli_query($database_connection, "delete from review where news_id=$news_id"); 
+     mysqli_query($database_connection, "delete from news where news_id=$news_id"); 
+     close_connection(); 
+
+     $message = "新闻及相关评论信息删除成功！"; 
+     header("Location:index.php?url=news_list.php&message=$message");
 } 
 ?> 
 <?php 
-include_once("functions/database.php"); 
-$news_id = $_GET["news_id"]; 
-get_connection(); 
-mysql_query("delete from review where news_id=$news_id"); 
-mysql_query("delete from news where news_id=$news_id"); 
-close_connection(); 
-$message = "新闻及相关评论信息删除成功！"; 
-header("Location:index.php?url=news_list.php&message=$message");
 ?> 
--- news_save.php
+++ news_save.php
@@ -1,35 +1,39 @@
 <?php 
 include_once("functions/is_login.php"); 
 include_once("functions/session_config.php"); 
+
 if(!is_login()){ 
      echo "请您登录系统后，再访问该页面！"; 
      return; 
-} 
+} else{
+     include_once("functions/file_system.php"); 
+     if(empty($_POST)){ 
+          $message = "上传的文件超过了php.ini中post_max_size选项限制的值"; 
+     }else{ 
+          $user_id = $_SESSION["user_id"]; 
+          $category_id = (isset($_POST["category_id"])? (intval($_POST["category_id"])>0?intval($_POST["category_id"]):1):1);
+          if(isset($_POST["title"])&&isset($_POST["content"])){
+               $title = htmlspecialchars(addslashes($_POST["title"])); 
+               $content = htmlspecialchars(addslashes($_POST["content"])); 
+               $currentDate =  date("Y-m-d H:i:s"); 
+               $clicked = 0; 
+               $file_name = $_FILES["news_file"]["name"]; 
+               $message = upload($_FILES["news_file"],"uploads"); #revisit this
 
-include_once("functions/file_system.php"); 
-if(empty($_POST)){ 
-     $message = "上传的文件超过了php.ini中post_max_size选项限制的值"; 
-}else{ 
-     $user_id = $_SESSION["user_id"]; 
-     $category_id = (isset($_POST["category_id"])? (intval($_POST["category_id"])>0?intval($_POST["category_id"]):1):1);
-     if(isset($_POST["title"])&&isset($_POST["content"])){
+               $sql = "insert into news values(null, $user_id, $category_id, '$title', '$content', '$currentDate', $clicked, '$file_name');"; 
 
-        $title = htmlspecialchars(addslashes()); 
-        $content = htmlspecialchars(addslashes($_POST["content"])); 
-        $currentDate =  date("Y-m-d H:i:s"); 
-        $clicked = 0; 
-        $file_name = $_FILES["news_file"]["name"]; 
-        $message = upload($_FILES["news_file"],"uploads"); 
-        $sql = "insert into news 
-               values(null,$user_id,$category_id,'$title','$content', '$currentDate',$clicked,'$file_name')"; 
-        if($message=="文件上传成功！"||$message=="没有选择上传附件！"){ 
-               include_once("functions/database.php"); 
-               get_connection(); 
-               $database_connection->query($sql); 
-               close_connection();		 
-        }
-     }	 
-} 
-$message = urlencode($message);
-header("Location:index.php?url=news_list.php&message=$message");  
+               if($message=="文件上传成功！"||$message=="没有选择上传附件！"){ 
+                         include_once("functions/database.php"); 
+                         get_connection(); 
+                         $database_connection->query($sql); 
+                         close_connection();		 
+               }
+          }	 
+     } 
+
+     $message = urlencode($message);
+     
+     header("Location:index.php?url=news_list.php&message=$message");  
+}
+
 ?>
