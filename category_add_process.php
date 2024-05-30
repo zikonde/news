c45e1cc5b03417f9bd8c7afc84dd4552978529ef
Zikonde Nyirenda <103549398+zikonde@users.noreply.github.com>
2024-05-30 17:31:43 +0800

    database

--- category_add_process.php
+++ category_add_process.php
@@ -1,4 +1,5 @@
 <?php 
+include_once("functions/database.php");
 include_once("functions/is_login.php"); 
 if(!is_login()){ 
      echo "请您登录后在访问该页面！"; 
@@ -11,7 +12,7 @@ $new_category = $_POST["new_category"];
 $new_description = $_POST["new_description"];  
 $sql = "update category set new_category=$new_category,description=$new_description"; 
 get_connection(); 
-mysql_query($sql); 
+mysqli_query($sql); 
 close_connection(); 
 header("Location:index.php?url=category_list.php"); 
 ?> 
