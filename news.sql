cb6e7a219e1014d7be7ff9269fdeb76038793346
Zikonde Nyirenda <103549398+zikonde@users.noreply.github.com>
2024-05-30 18:27:30 +0800

    Update news.sql

--- news.sql
+++ news.sql
@@ -9,6 +9,7 @@ use news;
 create table category( 
      category_id int auto_increment primary key, 
      name char(20) not null 
+     description char(255) not null
 ); 
 create table users( 
      user_id int auto_increment primary key, 
