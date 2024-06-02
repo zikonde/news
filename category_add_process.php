075ed8a9d57cf41299039b6d758ae9ac7c13b19f
Zikonde Nyirenda <103549398+zikonde@users.noreply.github.com>
2024-06-02 17:17:53 +0800

    Update category_add_process.php

--- category_add_process.php
+++ category_add_process.php
@@ -5,15 +5,19 @@ if($_SERVER["PHP_SELF"] === "/category_add_process.php"){
      if ($_SERVER["REQUEST_METHOD"] == "POST") {
           include_once("functions/database.php");
           include_once("functions/is_login.php"); 
+          include_once("url_navigator.php");
+
+
           if(!is_login()){ 
                echo "请您登录后在访问该页面！"; 
                return; 
           }else{
                include_once("functions/database.php"); 
+               
                $new_category = $_POST["new_category"]; 
                if(empty($new_category)){
                     $message = "请输入类别！"; 
-                    header("Location:index.php?url=category_add.php&message=$message"); 
+                    header("Location:".add_to_url(["message"=> $message]));
                }else{
                     $new_description = $_POST["new_description"];  
 
