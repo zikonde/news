11576ba88a0108e4e9c8ed1b2e9946ba9d1abd12
SchnappiDeng <1533677006@qq.com>
2024-06-10 19:14:15 +0800

    Update social-brands.php

--- social-brands.php
+++ social-brands.php
@@ -1,6 +1,6 @@
 
-<a href=""><i class="fab fa-weibo"></i></a>
-<a href=""><i class="fab fa-reddit-alien"></i></a>
+<a href="https://weibo.com/newssystem"><i class="fab fa-weibo"></i></a>
+<a alert()><i class="fab fa-reddit-alien"></i></a>
 <a href=""><i class="fab fa-weixin"></i></a>
 <a href=""><i class="fab fa-qq"></i></a>
 <a href=""><i class="fab fa-tiktok"></i></a>
